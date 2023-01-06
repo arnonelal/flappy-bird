e09262e3bb725bed18474b49642de27cd4ec2c9c
Arnon Elal <ar036el@gmail.com>
2023-01-06 04:52:02 +0200

    wip

---
+++ src/App/Game/Game.tsx
@@ -0,0 +1,32 @@
+import { Component } from 'react';
+import './Game.scss';
+
+
+
+interface Props {
+
+}
+
+interface State {
+
+}
+
+export default class Game extends Component<Props, State> {
+
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+
+  render() {
+    return (
+      <div id='Game'>
+
+      </div>
+    )
+  }
+}
+
