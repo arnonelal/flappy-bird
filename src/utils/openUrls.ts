4f22300f71adbfb7a3230653c2e5be9cb201f53a
Arnon Elal <ar036el@gmail.com>
2023-01-24 13:34:59 +0200

    wip

--- src/utils/openUrls.ts
+++ src/utils/openUrls.ts
@@ -1,9 +1,11 @@
 export function redirectToScoreSubmittionPage() {
   //fetch thingy here, and then-
-  window.open("https://www.google.com", "_top");
+  alert('notImplemented');
+  // window.open("https://www.google.com", "_top");
 }
 
 export function openSharePage() {
   //fetch thingy here, and then-
-  window.open("https://www.google.com", "_blank");
+  alert('notImplemented');
+  // window.open("https://www.google.com", "_blank");
 }
